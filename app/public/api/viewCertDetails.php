b729b1cbbc43e8bc0f3bf2401c556a1850866dcb
Matthew Lang <mattlang1997@gmail.com>
2020-10-16 17:31:14 -0400

    Update viewCertDetails.php

--- app/public/api/viewCertDetails.php
+++ app/public/api/viewCertDetails.php
@@ -2,20 +2,26 @@
 
 require 'common.php';
 
+// Step 1: Get a datase connection from our helper class
 $db = DbConnection::getConnection();
 
 // Step 2: Create & run the query
-// Note the use of parameterized statements to avoid injection
-$stmt = $db->prepare(
-  'insert into certification(nameShort, stdExpire) values(?, ?)'
-);
-try{
-  $stmt->execute([
-    $_POST['nameShort'],
-    $_POST['stdExpire']
-  ]);
-  echo "Certification Added Successfully";
-}
-catch (Exception $e){
-  echo "Something went wrong: ", $e->getMessage();
-}
+$sql = "select c.nameShort, c.stdExpire, concat(e.firstname, ' ', e.lastname) as fullname, ce.dateObtained, ce.dateExp
+from certification as c, employee as e, certEmpDetails as ce
+where ce.certID = c.certID and e.empID = ce.empID and c.nameShort = ?";
+
+
+
+$stmt = $db->prepare($sql);
+$stmt->execute([
+  $_GET['nameShort']
+]);
+
+$members = $stmt->fetchAll();
+
+// Step 3: Convert to JSON
+$json = json_encode($members, JSON_PRETTY_PRINT);
+
+// Step 4: Output
+header('Content-Type: application/json');
+echo $json;
