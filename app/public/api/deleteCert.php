be44ce63fe7efc7b456ed48b95e003e286ebb9f2
alexandernikas <alexandernikas1@gmail.com>
2020-11-01 21:49:57 -0500

    Completed certification html, js

--- app/public/api/deleteCert.php
+++ app/public/api/deleteCert.php
@@ -10,13 +10,15 @@ $db = DbConnection::getConnection();
 // Step 2: Create & run the query
 // Note the use of parameterized statements to avoid injection
 $stmt = $db->prepare(
-  'delete from certification where certID = ?'
+  'delete from certification where nameShort = ?'
 );
 try{
   $stmt->execute([
-    $_POST['certID']
+    $_GET['nameShort']
   ]);
   echo "Certification Successfully Deleted";
+  echo"<br>";
+  echo "<a href=\home.html>Home</a>";
 }
 catch (Exception $e){
   echo "Something went wrong: ", $e->getMessage();
